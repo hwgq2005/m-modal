db48500447ee2c940b2036683066b12386c812c1
hwgq2005 <262281610@qq.com>
2017-03-06 16:43:46 +0800

    页面修改

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,14 @@
  * @date    2017-03-06 13:38:52
  * @version 0.0.1
  */
-!function(window) {
+! function(window) {
 
 	'use strict';
 
 	var modalBtnOne = document.querySelector('#modal-one'),
 		modalBtnTwo = document.querySelector('#modal-two');
 
-	
-	modalBtnOne.onclick = function(){
+	modalBtnOne.onclick = function() {
 
 		var modal = new Modal({
 			id: 'modal',
@@ -22,19 +21,20 @@
 				// do something ...				
 			},
 			confirm: function() {
-				
+
 			},
 			cancel: function() {
-			
+
 			}
 		})
-	
+
 	}
 
-	modalBtnTwo.onclick = function(){
+	modalBtnTwo.onclick = function() {
+
 		new Modal({
 			id: 'modal1',
-			type:1,
+			type: 1,
 			title: '提示',
 			content: '操作成功',
 			complete: function() {
@@ -47,10 +47,9 @@
 				// do something ...
 			}
 		})
-		
-	
+
 	}
 
-
 	prettyPrint();
+	
 }(this);
